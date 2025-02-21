f51855f469eb55aacc50a4061906cf8d558caa17
Kevin Chen <kevin.chen234@gmail.com>
2025-02-21 15:33:49 -0800

    Add Models to enhance Task Manager - Added User - Added Tag - Updated database relationships between Task and User - Updated Database relationship between Tag and Tasks

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ApplicationRecord
-  has_many :tasks
+  has_many :tasks, dependent: :nullify
+
+  validates_length_of :name, within: 2..50
+
+  scope :sorted, -> { order(:name) }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_12_170926) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_21_231536) do
   create_table "categories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags_tasks", id: false, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "tag_id", null: false
+    t.bigint "task_id", null: false
+    t.index ["tag_id", "task_id"], name: "index_tags_tasks_on_tag_id_and_task_id"
+    t.index ["task_id", "tag_id"], name: "index_tags_tasks_on_task_id_and_tag_id"
+  end
+
+  create_table "task_assignments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "task_id", null: false
+    t.bigint "user_id", null: false
+    t.string "role"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["task_id", "user_id"], name: "index_task_assignments_on_task_id_and_user_id"
+    t.index ["task_id"], name: "index_task_assignments_on_task_id"
+    t.index ["user_id", "task_id"], name: "index_task_assignments_on_user_id_and_task_id"
+    t.index ["user_id"], name: "index_task_assignments_on_user_id"
   end
 
   create_table "tasks", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -27,4 +52,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_12_170926) do
     t.integer "category_id"
     t.index ["category_id"], name: "index_tasks_on_category_id"
   end
+
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "task_assignments", "tasks"
+  add_foreign_key "task_assignments", "users"
 end
